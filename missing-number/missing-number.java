e437f4b70423f894c6bd28312b0c2cab0b04b909
Pol Piñol Castuera <67922619+PolPinol@users.noreply.github.com>
2022-02-09 15:11:27 +0100

    Time: 8 ms, Memory: 51.1 MB - LeetHub

---
+++ missing-number/missing-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        
+        if (nums[0] != 0) {
+            return 0;
+        }
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i-1] != nums[i]-1) {
+                return nums[i-1]+1;
+            }
+        }
+        
+        return nums[nums.length-1]+1;
+    }
+}
