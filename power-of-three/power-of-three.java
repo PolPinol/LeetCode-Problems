4f81a151320de91474ec9f1ccd5f29d1d8965c2b
Pol Piñol Castuera <67922619+PolPinol@users.noreply.github.com>
2022-02-26 02:55:01 +0100

    Time: 19 ms, Memory: 48 MB - LeetHub

--- power-of-three/power-of-three.java
+++ power-of-three/power-of-three.java
@@ -1,11 +1,9 @@
 class Solution {
     public boolean isPowerOfThree(int n) {    
-        double d = Math.log10(n) / Math.log10(3);
-        
         if (n == 0) {
             return false;
         }
         
-        return d % 1 == 0;
+        return Math.log10(n) / Math.log10(3) % 1 == 0;
     }
 }
