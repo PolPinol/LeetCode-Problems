58d0b062d2456049596fc9d7682d7c4532ad7670
Pol Piñol Castuera <67922619+PolPinol@users.noreply.github.com>
2022-02-26 13:53:37 +0100

    Time: 2 ms, Memory: 42.4 MB - LeetHub

---
+++ valid-parentheses/valid-parentheses.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+                
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.empty()) {
+                    return false;
+                }
+                char opened = stack.pop().charValue();
+                if (c == ')') {
+                    if (opened != '(') {
+                        return false;
+                    }
+                } else if (c == '}') {
+                    if (opened != '{') {
+                        return false;
+                    }
+                } else {
+                    if (opened != '[') {
+                        return false;
+                    }
+                }
+            }
+        }
+        
+        return stack.empty();
+    }
+}
