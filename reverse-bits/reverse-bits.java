116cde5b1f7f55fa9932ea4c026c8313df07d827
Pol Piñol Castuera <67922619+PolPinol@users.noreply.github.com>
2022-02-28 15:34:13 +0100

    Time: 5 ms, Memory: 43 MB - LeetHub

--- reverse-bits/reverse-bits.java
+++ reverse-bits/reverse-bits.java
@@ -6,9 +6,6 @@ public class Solution {
         for (int i = 0; i < (32-binary.length()); i++) {
             reversed.append("0");
         }
-        System.out.println(reversed.toString());
-        
-        System.out.println(Long.parseLong(reversed.toString(),2));
         
         return (int) Long.parseLong(reversed.toString(),2);
     }
