39c93f288c4cfdb3b5540c91d66118502a45c4fd
Pol Piñol Castuera <67922619+PolPinol@users.noreply.github.com>
2022-03-06 19:05:15 +0100

    Time: 4 ms, Memory: 94.2 MB - LeetHub

--- increasing-triplet-subsequence/increasing-triplet-subsequence.java
+++ increasing-triplet-subsequence/increasing-triplet-subsequence.java
@@ -1,19 +1,17 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-        boolean solution = false;
         int min = Integer.MAX_VALUE;
         int mid = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
         
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] <= min) {
                 min = nums[i];
             } else if (nums[i] <= mid) {
                 mid = nums[i];
-            } else if (nums[i] > max) {
+            } else {
                 return true;
             }
-        }    
+        }
         
         return false;
     }
