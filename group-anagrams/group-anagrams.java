c0f49901252e5d94aa0061bbc12fdae955eae6c7
Pol Piñol Castuera <67922619+PolPinol@users.noreply.github.com>
2022-03-07 00:01:26 +0100

    Time: 15 ms, Memory: 55.9 MB - LeetHub

--- group-anagrams/group-anagrams.java
+++ group-anagrams/group-anagrams.java
@@ -1,28 +1,19 @@
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> solution = new ArrayList<>();
-        
-        if (strs.length == 0) {
-            return solution;
-        }
-        
-        HashMap<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
-        ArrayList<String> list = new ArrayList<String>();
+    public List<List<String>> groupAnagrams(String[] strs) {        
+        HashMap<String, ArrayList> map = new HashMap<String, ArrayList>();
         
         for (int i = 0; i < strs.length; i++) {
             char tempArray[] = strs[i].toCharArray();
             Arrays.sort(tempArray);
-            String aux = new String(tempArray);
+            String key = new String(tempArray);
             
-            if (map.containsKey(aux)) {
-                list = map.get(aux);
+            if (map.containsKey(key)) {
+                map.get(key).add(strs[i]);
             } else {
-                list = new ArrayList<String>();
+                map.put(key, new ArrayList(Arrays.asList(strs[i])));
             }
-            list.add(strs[i]);
-            map.put(new String(aux), list);
         }
         
-        return map.values().stream().collect(Collectors.toCollection(ArrayList::new));
+        return new ArrayList(map.values());
     }
 }
