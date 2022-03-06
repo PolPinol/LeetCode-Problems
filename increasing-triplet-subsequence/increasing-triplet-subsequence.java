05e9c148f42243ee73c987d33f92c79adb68c3b9
Pol Piñol Castuera <67922619+PolPinol@users.noreply.github.com>
2022-03-06 19:03:37 +0100

    Time: 3 ms, Memory: 94.3 MB - LeetHub

--- increasing-triplet-subsequence/increasing-triplet-subsequence.java
+++ increasing-triplet-subsequence/increasing-triplet-subsequence.java
@@ -6,17 +6,15 @@ class Solution {
         int max = Integer.MIN_VALUE;
         
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] < min) {
+            if (nums[i] <= min) {
                 min = nums[i];
-            } else if (nums[i] < mid && nums[i] != min) {
+            } else if (nums[i] <= mid) {
                 mid = nums[i];
-            } else if (nums[i] > max && nums[i] != mid && nums[i] != min) {
+            } else if (nums[i] > max) {
                 max = nums[i];
             }
-            System.out.println(min + "," + mid + "," + max);
         }
         
-        System.out.println(min + "," + mid + "," + max);
         
         if (min < mid && mid < max && 
             min != Integer.MAX_VALUE && mid != Integer.MAX_VALUE && max != Integer.MIN_VALUE) {
