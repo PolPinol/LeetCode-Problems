98a4bb9f1853879b7e80ac7d769774e4e0157fb5
Pol Piñol Castuera <67922619+PolPinol@users.noreply.github.com>
2022-03-06 22:34:18 +0100

    Time: 4 ms, Memory: 53.9 MB - LeetHub

--- set-matrix-zeroes/set-matrix-zeroes.java
+++ set-matrix-zeroes/set-matrix-zeroes.java
@@ -5,7 +5,6 @@ class Solution {
         
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
-                System.out.print(matrix[i][j]);
                 if (rows[i] == 1 || columns[j] == 1) {
                     if (matrix[i][j] == 0) {
                         columns[j] = 1;
