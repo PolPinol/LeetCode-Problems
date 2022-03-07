4e7f335025458e0ff93f7ff465aa457bf397f4ad
Pol Piñol Castuera <67922619+PolPinol@users.noreply.github.com>
2022-03-07 15:39:49 +0100

    Time: 4 ms (50.36%), Space: 56.8 MB (13.69%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,28 +1,5 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        
-        // First approach
-        /*
-        int n = nums.length;
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        
-        for (int i = 0; i < n; i++) {
-            int count = 0;
-            if (map.containsKey(nums[i])) {
-                count = map.get(nums[i]);
-            }
-            map.put(nums[i], count+1);
-        }
-        
-        for (Integer i : map.keySet()) {
-            if (map.get(i) > n/2) {
-                return i;
-            }
-        }
-        
-        return -1;
-        */
-        
         // Second approach 
         Arrays.sort(nums);
         return nums[nums.length/2];
